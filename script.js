5e2e65dc4c4e3786309e92b4d2dd60e08a621ee9
A. Mirelle Wallgren <mirelle.wallgren@medieinstitutet.se>
2022-11-27 21:54:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+// hämtar element från login-rutan
+const userNameInput = document.getElementById('userNameInput');
+const passwordInput = document.getElementById('passwordInput');
+const loginBtn = document.getElementById('loginBtn');
+
+// hämtar element från signup-rutan
+const signUpBtn = document.getElementById('signUpBtn');
+const newUserName = document.getElementById('newUserName');
+const newPassword = document.getElementById('newPassword');
+
+// div som skriver ut meddelanden på sidan
+const demo = document.getElementById('demo');
+
+if(localStorage.getItem('users')) {
+    console.log('');
+} else {
+    let users = [ 
+        {id: 0, userName: "janne", password: "test"},
+        {id: 1, userName: "mirr", password: "123"},
+        {id: 2, userName: "cheri", password: "skinka"}
+     ]
+    localStorage.setItem('users', JSON.stringify(users));
+}
+
+if(localStorage.getItem('loggedInUser')) {
+    printUserName();
+}
+
+ loginBtn.addEventListener('click', () => {
+    let users = JSON.parse(localStorage.getItem('users'));
+    let user = users.find(user => user.userName === userNameInput.value && user.password === passwordInput.value); 
+  //  console.log("janne", user)
+
+     if (user) {
+
+        let loggedInUser = userNameInput.value;
+        localStorage.setItem('loggedInUser', loggedInUser);
+
+        printUserName();
+
+    } else {
+       userDoesNotExist();
+       console.log('vill inte visa sig');
+    } 
+}); 
+
+//////////////
+// FUNKTIONER
+function printUserName() {
+
+    let loggedInUser = localStorage.getItem('loggedInUser');
+    demo.innerText = 'Välkommen! Du är inloggad som: ' + loggedInUser + ' ';
+
+    // skapar en logga-ut-knapp
+    let logoutBtn = document.createElement("button")
+    logoutBtn.innerText = "Logga ut!";
+    demo.appendChild(logoutBtn);
+
+    logoutBtn.addEventListener("click", () => {
+      localStorage.removeItem("loggedInUser"); 
+      demo.innerText = 'Du är nu utloggad ';
+    })
+}
+
+function userDoesNotExist() {
+    demo.innerText = 'Användarnamn eller lösenord stämmer inte ';
+}
+
+// för att spara nya användare:
+signUpBtn.addEventListener('click', () => {
+   // console.log('klick på knapp'); 
+    let users = JSON.parse(localStorage.getItem('users'));
+
+    let newUser = {
+        id: users.length + 1,
+        userName: newUserName.value,
+        password: newPassword.value
+
+    }
+ //   console.log('det funkar', newUser); 
+    demo.innerText = 'Grattis till ditt nya medlemsskap! ' + 
+    ' Prova att logga in nu, ' + newUser.userName + ' ;) ';
+
+    users.push(newUser);
+    localStorage.setItem('users', JSON.stringify(users));
+});
